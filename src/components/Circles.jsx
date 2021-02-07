0e9f0d3b280aec7010dbd5e84b1ee2e6a7189f9f
RainFlo <flowingwithrain@gmail.com>
2021-02-06 23:52:48 -0500

    initial commit

---
+++ src/components/Circles.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+function Circles() {
+    return (
+        <div>
+            <h2>Circles</h2>
+            <p><b>Score: </b>0</p>
+        </div>
+    )
+}
+
+export default Circles
